096a65821a7efea398d7a1a49d4d8426d6b0bc3d
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2024-08-13 23:00:56 +1000

    Testing a simplified FixedSupplyToken

--- contracts/FixedSupplyToken.sol
+++ contracts/FixedSupplyToken.sol
@@ -1,7 +1,3 @@
-/**
- *Submitted for verification at Etherscan.io on 2024-08-13
-*/
-
 pragma solidity ^0.8.0;
 
 // ----------------------------------------------------------------------------
@@ -9,15 +5,14 @@ pragma solidity ^0.8.0;
 //
 // https://github.com/bokkypoobah/GettingStartedInEthereum
 //
-// Deployed to Sepolia 0x8fAF86fa6B137B316D699e23Ae1DcA3C12e7d093
-//
-// https://eips.ethereum.org/EIPS/eip-20
+// Deployed to Sepolia
 //
 // SPDX-License-Identifier: MIT
 //
 // Enjoy. (c) BokkyPooBah / Bok Consulting Pty Ltd 2024. The MIT Licence.
 // ----------------------------------------------------------------------------
 
+// https://eips.ethereum.org/EIPS/eip-20
 interface IERC20 {
     event Transfer(address indexed from, address indexed to, uint tokens);
     event Approval(address indexed owner, address indexed spender, uint tokens);
@@ -29,53 +24,46 @@ interface IERC20 {
     function totalSupply() external view returns (uint);
     function balanceOf(address owner) external view returns (uint balance);
     function allowance(address owner, address spender) external view returns (uint remaining);
+
     function transfer(address to, uint tokens) external returns (bool success);
     function approve(address spender, uint tokens) external returns (bool success);
     function transferFrom(address from, address to, uint tokens) external returns (bool success);
 }
 
 contract FixedSupplyToken is IERC20 {
-    string public symbol;
-    string public name;
-    uint8 public decimals;
-    uint _totalSupply;
+    string constant public symbol = "HIHIHI";
+    string constant public name = unicode"Hi 👋, Hi 👋, Hi 👋";
+    uint8 constant public decimals = 18;
+    uint constant public _totalSupply= 1_000_000_000_000_000_000_000_000; // 1,000,000.000000000000000000
 
-    mapping(address => uint) balances;
-    mapping(address => mapping(address => uint)) allowed;
+    // owner => balance
+    mapping(address => uint) public balanceOf;
+    // owner => spender => tokens
+    mapping(address => mapping(address => uint)) public allowance;
 
     constructor() {
-        symbol = "GMGMGM";
-        name = unicode"Good Morning 👋, Good Morning 👋, Good Morning 👋";
-        decimals = 18;
-        _totalSupply = 1_000_000_000_000_000_000_000_000; // 1 million
-        balances[msg.sender] = _totalSupply;
+        balanceOf[msg.sender] = _totalSupply;
         emit Transfer(address(0), msg.sender, _totalSupply);
     }
     function totalSupply() external view override returns (uint) {
-        return _totalSupply - balances[address(0)];
-    }
-    function balanceOf(address owner) external view override returns (uint balance) {
-        return balances[owner];
+        return _totalSupply - balanceOf[address(0)];
     }
     function transfer(address to, uint tokens) external override returns (bool success) {
-        balances[msg.sender] -= tokens;
-        balances[to] += tokens;
+        balanceOf[msg.sender] -= tokens;
+        balanceOf[to] += tokens;
         emit Transfer(msg.sender, to, tokens);
         return true;
     }
     function approve(address spender, uint tokens) external override returns (bool success) {
-        allowed[msg.sender][spender] = tokens;
+        allowance[msg.sender][spender] = tokens;
         emit Approval(msg.sender, spender, tokens);
         return true;
     }
     function transferFrom(address from, address to, uint tokens) external override returns (bool success) {
-        balances[from] -= tokens;
-        allowed[from][msg.sender] -= tokens;
-        balances[to] += tokens;
+        balanceOf[from] -= tokens;
+        allowance[from][msg.sender] -= tokens;
+        balanceOf[to] += tokens;
         emit Transfer(from, to, tokens);
         return true;
-    }
-    function allowance(address owner, address spender) external view override returns (uint remaining) {
-        return allowed[owner][spender];
     }
 }
---
+++ test/00_test_fixedSupplyToken.js
@@ -0,0 +1,188 @@
+const { time, loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { expect } = require("chai"); // https://hardhat.org/hardhat-chai-matchers/docs/reference
+
+describe("00_test_0", function () {
+  async function deployContracts() {
+    const FIXEDSUPPLYTOKEN = {
+      SYMBOL: "HIHIHI",
+      NAME: "Hi 👋, Hi 👋, Hi 👋",
+      DECIMALS: 18,
+      TOTALSUPPLY: "1000000000000000000000000",
+    };
+
+    const accounts = await ethers.getSigners();
+    console.log("        Accounts: " + JSON.stringify(accounts.slice(0, 2).map(e => e.address)));
+
+    console.log("        Deploying FixedSupplyToken");
+    const FixedSupplyToken = await ethers.getContractFactory("FixedSupplyToken");
+    const fixedSupplyToken = await FixedSupplyToken.deploy();
+    console.log("        * symbol: " + await fixedSupplyToken.symbol());
+    console.log("        * name: " + await fixedSupplyToken.name());
+    console.log("        * decimals: " + await fixedSupplyToken.decimals());
+    console.log("        * totalSupply: " + await fixedSupplyToken.totalSupply());
+
+    return { FIXEDSUPPLYTOKEN, accounts, fixedSupplyToken };
+  }
+
+  describe("Deployment", function () {
+
+    it("FixedSupplyToken should have the correct symbol, name, decimals, totalSupply and balanceOf", async function () {
+      const { FIXEDSUPPLYTOKEN, accounts, fixedSupplyToken } = await loadFixture(deployContracts);
+      expect(await fixedSupplyToken.symbol()).to.equal(FIXEDSUPPLYTOKEN.SYMBOL);
+      expect(await fixedSupplyToken.name()).to.equal(FIXEDSUPPLYTOKEN.NAME);
+      expect(await fixedSupplyToken.decimals()).to.equal(FIXEDSUPPLYTOKEN.DECIMALS);
+      expect(await fixedSupplyToken.totalSupply()).to.equal(FIXEDSUPPLYTOKEN.TOTALSUPPLY);
+      expect(await fixedSupplyToken.balanceOf(accounts[0])).to.equal(FIXEDSUPPLYTOKEN.TOTALSUPPLY);
+      expect(await fixedSupplyToken.balanceOf(accounts[1])).to.equal(0);
+    });
+
+    // it("ERC20 token should emit an event on transfers and balanceOf adds up", async function () {
+    //   const { erc20Token, owner, account2 } = await loadFixture(deployContracts);
+    //
+    //   await expect(erc20Token.transfer(account2, "1"))
+    //     .to.emit(erc20Token, "Transfer")
+    //     .withArgs(owner, account2, anyValue);
+    //   expect(await erc20Token.balanceOf(owner)).to.equal("999999999999999999999999");
+    //   expect(await erc20Token.balanceOf(account2)).to.equal("1");
+    //
+    //   await expect(erc20Token.transfer(account2, "2"))
+    //     .to.emit(erc20Token, "Transfer")
+    //     .withArgs(owner, account2, "2");
+    //   expect(await erc20Token.balanceOf(owner)).to.equal("999999999999999999999997");
+    //   expect(await erc20Token.balanceOf(account2)).to.equal("3");
+    //
+    // });
+
+    // it("SimpleVault should process token deposits and withdrawals correctly", async function () {
+    //   const { erc20Token, simpleVault, owner, otherAccount, TOTALSUPPLY } = await loadFixture(deployContracts);
+    //
+    //   await expect(simpleVault.depositTokens(erc20Token, "1")).to.be.revertedWithPanic(0x11);
+    //
+    //   await expect(erc20Token.approve(simpleVault, "100"))
+    //     .to.emit(erc20Token, "Approval")
+    //     .withArgs(owner, simpleVault, "100");
+    //
+    //   await expect(simpleVault.depositTokens(erc20Token, "1"))
+    //     .to.emit(erc20Token, "Transfer")
+    //     .withArgs(owner, simpleVault, "1")
+    //     .to.emit(simpleVault, "TokensDeposited")
+    //     .withArgs(owner, erc20Token, "1");
+    //
+    //   expect(await erc20Token.balanceOf(owner)).to.equal("999999999999999999999999");
+    //   expect(await erc20Token.balanceOf(simpleVault)).to.equal("1");
+    //
+    //   await expect(simpleVault.connect(otherAccount).withdrawTokens(erc20Token, "1")).to.be.reverted;
+    //
+    //   const withdrawTokens = await simpleVault.withdrawTokens(erc20Token, "1");
+    //   expect(withdrawTokens)
+    //     .to.emit(erc20Token, "Transfer")
+    //     .withArgs(simpleVault, owner, "1")
+    //     .to.emit(simpleVault, "TokensWithdrawn")
+    //     .withArgs(owner, erc20Token, "1");
+    //   // console.log("withdrawTokens: " + JSON.stringify(withdrawTokens, null, 2));
+    //   // const receipt = await withdrawTokens.wait();
+    //   // console.log("receipt: " + JSON.stringify(receipt, null, 2));
+    //
+    //   expect(await erc20Token.balanceOf(owner)).to.equal(TOTALSUPPLY);
+    //   expect(await erc20Token.balanceOf(simpleVault)).to.equal("0");
+    // });
+
+    // it("Should set the right unlockTime", async function () {
+    //   const { lock, unlockTime } = await loadFixture(deployContracts);
+    //
+    //   expect(await lock.unlockTime()).to.equal(unlockTime);
+    // });
+    //
+    // it("Should set the right owner", async function () {
+    //   const { lock, owner } = await loadFixture(deployContracts);
+    //
+    //   expect(await lock.owner()).to.equal(owner.address);
+    // });
+    //
+    // it("Should receive and store the funds to lock", async function () {
+    //   const { lock, lockedAmount } = await loadFixture(
+    //     deployContracts
+    //   );
+    //
+    //   expect(await ethers.provider.getBalance(lock.target)).to.equal(
+    //     lockedAmount
+    //   );
+    // });
+    //
+    // it("Should fail if the unlockTime is not in the future", async function () {
+    //   // We don't use the fixture here because we want a different deployment
+    //   const latestTime = await time.latest();
+    //   const Lock = await ethers.getContractFactory("Lock");
+    //   await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
+    //     "Unlock time should be in the future"
+    //   );
+    // });
+  });
+
+  // describe("Withdrawals", function () {
+  //   describe("Validations", function () {
+  //     it("Should revert with the right error if called too soon", async function () {
+  //       const { lock } = await loadFixture(deployContracts);
+  //
+  //       await expect(lock.withdraw()).to.be.revertedWith(
+  //         "You can't withdraw yet"
+  //       );
+  //     });
+  //
+  //     it("Should revert with the right error if called from another account", async function () {
+  //       const { lock, unlockTime, otherAccount } = await loadFixture(
+  //         deployContracts
+  //       );
+  //
+  //       // We can increase the time in Hardhat Network
+  //       await time.increaseTo(unlockTime);
+  //
+  //       // We use lock.connect() to send a transaction from another account
+  //       await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
+  //         "You aren't the owner"
+  //       );
+  //     });
+  //
+  //     it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
+  //       const { lock, unlockTime } = await loadFixture(
+  //         deployContracts
+  //       );
+  //
+  //       // Transactions are sent using the first signer by default
+  //       await time.increaseTo(unlockTime);
+  //
+  //       await expect(lock.withdraw()).not.to.be.reverted;
+  //     });
+  //   });
+  //
+  //   describe("Events", function () {
+  //     it("Should emit an event on withdrawals", async function () {
+  //       const { lock, unlockTime, lockedAmount } = await loadFixture(
+  //         deployContracts
+  //       );
+  //
+  //       await time.increaseTo(unlockTime);
+  //
+  //       await expect(lock.withdraw())
+  //         .to.emit(lock, "Withdrawal")
+  //         .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
+  //     });
+  //   });
+  //
+  //   describe("Transfers", function () {
+  //     it("Should transfer the funds to the owner", async function () {
+  //       const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
+  //         deployContracts
+  //       );
+  //
+  //       await time.increaseTo(unlockTime);
+  //
+  //       await expect(lock.withdraw()).to.changeEtherBalances(
+  //         [owner, lock],
+  //         [lockedAmount, -lockedAmount]
+  //       );
+  //     });
+  //   });
+  // });
+});
