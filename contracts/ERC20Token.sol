6e2b7844e8a25db5a8616fb29450e7bc15b05261
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2024-08-14 01:03:37 +1000

    ERC20Token tidy

--- contracts/ERC20Token.sol
+++ contracts/ERC20Token.sol
@@ -7,17 +7,16 @@ pragma solidity ^0.8.0;
 // ----------------------------------------------------------------------------
 // ERC-20 Token
 //
+// Deployed to Sepolia 
+//
 // https://github.com/bokkypoobah/GettingStartedInEthereum
-//
-// Deployed to Sepolia 0x1e5Df6dB242d07cc40a37b634022c02F73A74D59
-//
-// https://eips.ethereum.org/EIPS/eip-20
 //
 // SPDX-License-Identifier: MIT
 //
 // Enjoy. (c) BokkyPooBah / Bok Consulting Pty Ltd 2024. The MIT Licence.
 // ----------------------------------------------------------------------------
 
+// https://eips.ethereum.org/EIPS/eip-20
 interface IERC20 {
     event Transfer(address indexed from, address indexed to, uint tokens);
     event Approval(address indexed owner, address indexed spender, uint tokens);
@@ -25,10 +24,10 @@ interface IERC20 {
     function name() external view returns (string memory);
     function symbol() external view returns (string memory);
     function decimals() external view returns (uint8);
-
     function totalSupply() external view returns (uint);
     function balanceOf(address owner) external view returns (uint balance);
     function allowance(address owner, address spender) external view returns (uint remaining);
+
     function transfer(address to, uint tokens) external returns (bool success);
     function approve(address spender, uint tokens) external returns (bool success);
     function transferFrom(address from, address to, uint tokens) external returns (bool success);
