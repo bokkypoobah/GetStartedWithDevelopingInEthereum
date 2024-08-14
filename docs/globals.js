3f3b2044d10d84b82c279e50dfe839a8528cffdf
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2024-08-15 00:53:23 +1000

    tool nonFungible modal wip

--- docs/globals.js
+++ docs/globals.js
@@ -1,6 +1,7 @@
 const ADDRESS0 = "0x0000000000000000000000000000000000000000";
 const generateRange = (start, stop, step) => Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step));
 const delay = ms => new Promise(res => setTimeout(res, ms));
+const now = () => moment().format("HH:mm:ss");
 function handleErrors(response) {
   if (!response.ok) {
     throw Error(response.statusText);
