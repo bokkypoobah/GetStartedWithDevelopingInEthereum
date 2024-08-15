69ff2a5a0a7049bbb70218f45ec5ccafb83f0c96
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2024-08-15 12:04:57 +1000

    tool - sample contracts

--- docs/networks.js
+++ docs/networks.js
@@ -9,6 +9,14 @@ const NETWORKS = {
         "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${contract}/logo.png",
     ],
     reservoir: "https://api.reservoir.tools/",
+    contracts: {
+      "0x2823589Ae095D99bD64dEeA80B4690313e2fB519": { type: "erc20", name: "WEENUS" },
+      "0xeEf5E2d8255E973d587217f9509B416b41CA5870": { type: "erc20", name: "XEENUS" },
+      "0x187E63F9eBA692A0ac98d3edE6fEb870AF0079e1": { type: "erc20", name: "YEENUS" },
+      "0x0693c3a780A0a757E803a4BD76bCf43d438f8806": { type: "erc20", name: "ZEENUS" },
+      "0x8fa600364b93c53e0c71c7a33d2ade21f4351da3": { type: "erc721", name: "Larva Chads" },
+      "0xFe9231f0e6753a8412a00eC1f0028A24d5220Ba9": { type: "erc1155", name: "Zombie Babies" },
+    },
   },
   42161: {
     name: "Arbitrum",
@@ -78,6 +86,15 @@ const NETWORKS = {
         "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/sepolia/assets/${contract}/logo.png",
     ],
     reservoir: "https://api-sepolia.reservoir.tools/",
+    contracts: {
+      "0x7439E9Bb6D8a84dd3A23fe621A30F95403F87fB9": { type: "erc20", name: "WEENUS" },
+      "0xc21d97673B9E0B3AA53a06439F71fDc1facE393B": { type: "erc20", name: "XEENUS" },
+      "0x93fCA4c6E2525C09c95269055B46f16b1459BF9d": { type: "erc20", name: "YEENUS" },
+      "0xe9EF74A6568E9f0e42a587C9363C9BcC582dcC6c": { type: "erc20", name: "ZEENUS" },
+      "0x1e5df6db242d07cc40a37b634022c02f73a74d59": { type: "erc20", name: "MYERC20TOKEN" },
+      "0x8b73448426797099b6b9a96c4343f528bbAfc55e": { type: "erc721", name: "TESTTOADZ" },
+      "0x3F15A716888EFb6871872fC4358F638DEE495f3b": { type: "erc1155", name: "RANDOM" },
+    },
   },
   17000: {
     name: "Holešky Testnet",
